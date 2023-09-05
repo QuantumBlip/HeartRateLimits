e5ac55eb0728c3caa95e3f36ef5443f53531494e
t1kike00 <t1kike00@students.oamk.fi>
2023-09-05 17:11:06 +0300

    Näyttää ja päivittää luvut oikein

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [age, setAge] = useState('')
+  const [upperLimit, setUpperLimit] = useState(0)
+  const [lowerLimit, setLowerLimit] = useState(0)
+
+  calc = a => {
+    setAge(a)
+    let x = (220-Number(a))
+    let upper = x*0.85
+    let lower = x*0.65
+
+    setUpperLimit(Math.round(upper))
+    setLowerLimit(Math.round(lower))
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Heart Rate Limits based by Age.</Text>
+      <TextInput placeholder='Enter Age in years'
+      keyboardType='decimal-pad'
+      value={age} onChangeText={text => calc(text)}
+      style={styles.field}
+      />
+
+    <Text style={styles.field}>Hr limits</Text> 
+    <Text style={styles.field}>{lowerLimit.toString()} - {upperLimit.toString()}</Text>
     </View>
   );
 }
